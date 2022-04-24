843848c98c9d164339265db0f0fcb947b8148104
scrafet <scrafet@gmail.com>
2022-04-24 13:18:33 -0500

    clase 16

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from core.erp.models import Category, Product
+
+
+def myfirstview(request):
+    data = {
+        'name': 'william',
+        'categories': Category.objects.all()
+    }
+    # return HttpResponse('hola esta es mi  primera url')
+    # return JsonResponse(data)
+    return render(request, 'index.html', data)
+
+def mysecondview(request):
+    data = {
+        'name': 'william',
+        'products': Product.objects.all()
+    }
+    # return HttpResponse('hola esta es mi  primera url')
+    # return JsonResponse(data)
+    return render(request, 'second.html', data)
