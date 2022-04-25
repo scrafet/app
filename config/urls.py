068f409e231df84cb4fefd510940fca6d6731e81
scrafet <scrafet@gmail.com>
2022-04-25 10:12:38 -0500

    clase 19 -implementacion plantilla list.html -

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core.erp.views import myfirstview
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('prueba/', include('core.erp.urls')),
+    path('erp/', include('core.erp.urls')),
 
 ]
